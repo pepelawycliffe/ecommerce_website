550b60faa29ce377d45568c8d7911c9618ee0340
wycliffepepela <wycliffepepelacrash@gmail.com>
2021-03-17 14:16:39 +0300

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2020 Brought To You By<a href="https://wycliffepepela.github.io/">Wycliffe</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought to You By <a href="https://wycliffepepela.github.io/">Wycliffe</a></strong>
+    </div>
+</footer>
